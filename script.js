5c12db4e0f2668ad6149c50c40391ea794b385bb
krico730 <lega12016@liub.ukr.education>
2025-03-03 02:32:30 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("no").addEventListener("mouseover", function() {
+    this.style.top = Math.random() * window.innerHeight + "px";
+    this.style.left = Math.random() * window.innerWidth + "px";
+});
